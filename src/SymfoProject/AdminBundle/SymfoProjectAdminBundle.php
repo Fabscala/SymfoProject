030fbce7260d8312d8f0ed15f7c2201a8dcd6216
Fabien Scalabrin <fscalabrin2@gmail.com>
2017-01-31 09:16:20 +0100

    suppression car mauvais nom

--- src/SymfoProject/AdminBundle/SymfoProjectAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SymfoProject\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfoProjectAdminBundle extends Bundle
-{
-}
--- src/SymfoProject/CinemaBundle/SymfoProjectCinemaBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SymfoProject\CinemaBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfoProjectCinemaBundle extends Bundle
-{
-}
