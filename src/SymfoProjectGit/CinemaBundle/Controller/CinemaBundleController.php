164bd276fc5256064f4d097013f04551b9242b5a
Fabien Scalabrin <fscalabrin2@gmail.com>
2017-01-31 09:34:08 +0100

    modification des routes de base

--- src/SymfoProjectGit/CinemaBundle/Controller/CinemaBundleController.php
+++ src/SymfoProjectGit/CinemaBundle/Controller/CinemaBundleController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
     {
         return $this->render('SymfoProjectGitCinemaBundle:Default:index.html.twig');
     }
+
+    /**
+    * @Route("/film")
+    */
+    public function listAction()
+    {
+        return $this->render('SymfoProjectGitCinemaBundle:Film:list.html.twig');
+    }
+
+    /**
+    * @Route("/film/{id}", requirements={"id": "\d+"})
+    */
+    public function showAction($id)
+    {
+        return $this->render('SymfoProjectGitCinemaBundle:Film:show.html.twig');
+    }
 }
